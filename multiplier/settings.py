37b10222b4fc3072ab51fde52c136ae23a441744
Ramees AR <62797124+Ramees12ar@users.noreply.github.com>
2020-12-05 19:40:55 +0530

    Update settings.py

--- multiplier/settings.py
+++ multiplier/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&l$2wr98+0c7h48l35^sauv@eupfhcjo6l9a()w7te**t$3h%2'
+SECRET_KEY = ["type your secret key"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
